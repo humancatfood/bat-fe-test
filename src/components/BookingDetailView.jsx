beff16c6c96d8f4ef178ef79dcd3466815f05885
Jan Borchers <jan@osper.com>
2017-06-01 18:18:29 +0100

    Factor BookingForm into editable component

--- src/components/BookingDetailView.jsx
+++ src/components/BookingDetailView.jsx
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 
 import { selectBooking } from './../data/actions';
 
+import BookingForm from './BookingForm';
+
+
 
 @connect(store => ({
   booking: store.bookings.byId[store.ui.selectedBookingId]
@@ -19,54 +22,17 @@ export default class BookingDetailView extends React.Component
     {
       return null;
     }
-
-    const seated = booking.cancelled ?
-      'cancelled' :
-      booking.seated ?
-        'yes' :
-        'no';
-
-    return (
-      <section className="bookings-detail-view">
-        <h2 className="bui-app-intro">
-          Booking update:
-          <button onClick={ () => selectBooking(undefined) } style={{float: 'right'}}>X</button>
-        </h2>
-
-        <form>
-          <fieldset>
-            <legend>Name</legend>
-            <select name="title" onChange={console.log}>
-              {
-                ['Mr', 'Mrs', 'Miss', 'Ms', 'Dr'].map(title => (
-                  <option key={ title } value="{ title }" checked={ title === booking.title}>{ title }</option>
-                ))
-              }
-            </select>
-            <input type="text" name="first-name" value={ booking.firstName } onChange={e => console.log(arguments)}/>
-            <input type="text" name="title" value={ booking.lastName } onChange={e => console.log(arguments)}/>
-          </fieldset>
-          <fieldset>
-            <legend>Time</legend>
-            <input type="time" name="time" value={ booking.time } />
-          </fieldset>
-          <fieldset>
-            <legend>Covers</legend>
-            <input type="number" name="covers" value={ booking.partySize } />
-          </fieldset>
-          <fieldset>
-            <legend>Seated</legend>
-            <label><input type="radio" name="seated" value="no" checked={ seated === 'no'} />No</label>
-            <label><input type="radio" name="seated" value="yes" checked={ seated === 'yes'} />Yes</label>
-            <label><input type="radio" name="seated" value="cancelled" checked={ seated === 'cancelled'} />Cancelled</label>
-          </fieldset>
-          <fieldset>
-            <legend>Notes</legend>
-            <textarea name="notes" rows="4" value={ booking.notes }></textarea>
-          </fieldset>
-        </form>
-      </section>
-    );
+    else {
+      return (
+        <section className="bookings-detail-view">
+          <h2 className="bui-app-intro">
+            Booking update:
+            <button onClick={ () => selectBooking(undefined) } style={{float: 'right'}}>X</button>
+          </h2>
+          <BookingForm booking={ booking }/>
+        </section>
+      );
+    }
 
   }
 }
---
+++ src/components/BookingForm.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectBooking } from './../data/actions';
+
+
+
+@connect(store => ({
+  booking: store.bookings.byId[store.ui.selectedBookingId]
+}), {
+  selectBooking
+})
+export default class BookingForm extends React.Component
+{
+
+  constructor (props, ...rest)
+  {
+    super(props, ...rest);
+
+    this._onChange = this._onChange.bind(this);
+    this._onReset = this._onReset.bind(this);
+    this._onSubmit = this._onSubmit.bind(this);
+
+    this.state = {
+      dirty: false,
+      ...this._propsToState(props)
+    };
+
+  }
+
+  componentWillReceiveProps (nextProps)
+  {
+    if (nextProps.booking.id !== this.props.booking.id)
+    {
+      this.setState({
+        dirty: false,
+        ...this._propsToState(nextProps)
+      });
+    }
+  }
+
+  _onChange (e)
+  {
+    this.setState({
+      dirty: true,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  _onReset ()
+  {
+    this.setState({
+      dirty: false,
+      ...this._propsToState(this.props)
+    });
+  }
+
+  _onSubmit (event)
+  {
+    event.preventDefault();
+    console.log("submitting:", this.state);
+  }
+
+  _propsToState (props)
+  {
+    const { booking } = props;
+    return {
+      ...booking,
+      status: booking.cancelled ? 'cancelled' : booking.seated ? 'yes' : 'no',
+      time: booking.time.replace('.', ':')
+    };
+  }
+
+  render ()
+  {
+    const { dirty, title, firstName, lastName, time, partySize, status, notes} = this.state;
+
+    return (
+      <form onChange={ this._onChange }
+            onSubmit={ this._onSubmit }
+            onReset={ this._onReset }>
+        <fieldset>
+          <legend>Name</legend>
+          <select name="title" value={ title } onChange={ this._onChange } >
+            {
+              ['Mr', 'Mrs', 'Miss', 'Ms', 'Dr'].map(abbr => (
+                <option key={ abbr } value={ abbr }>{ abbr }</option>
+              ))
+            }
+          </select>
+          <input type="text" name="firstName" value={ firstName } onChange={ this._onChange } />
+          <input type="text" name="lastName" value={ lastName } onChange={ this._onChange } />
+        </fieldset>
+        <fieldset>
+          <legend>Time</legend>
+          <input type="time" name="time" value={ time } onChange={ this._onChange } />
+        </fieldset>
+        <fieldset>
+          <legend>Covers</legend>
+          <input type="number" name="partySize" value={ partySize } onChange={ this._onChange } />
+        </fieldset>
+        <fieldset>
+          <legend>Seated</legend>
+          <label><input type="radio" name="status" value="no" onChange={ this._onChange } checked={ status == 'no' } />No</label>
+          <label><input type="radio" name="status" value="yes" onChange={ this._onChange } checked={ status == 'yes' } />Yes</label>
+          <label><input type="radio" name="status" value="cancelled" onChange={ this._onChange } checked={ status == 'cancelled' } />Cancelled</label>
+        </fieldset>
+        <fieldset>
+          <legend>Notes</legend>
+          <textarea name="notes" rows="4" value={ notes } onChange={ this._onChange } ></textarea>
+        </fieldset>
+
+        <fieldset>
+          <button type="submit" disabled={ !dirty }>Save Changes</button>
+          <button type="reset" disabled={ !dirty }>Cancel</button>
+        </fieldset>
+
+      </form>
+    );
+
+  }
+}
