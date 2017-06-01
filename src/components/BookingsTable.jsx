ec8a0e2bcbfd1d6a698f7ff8cc2a4feb0689b729
Jan Borchers <jan@osper.com>
2017-06-01 20:00:31 +0100

    Factor BookingsTable into a smart component

--- src/components/BookingsTable.jsx
+++ src/components/BookingsTable.jsx
@@ -1,34 +1,56 @@
 import React from 'react';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+
+import { selectBooking } from './../data/actions';
 
 
 
-export default ({ bookings, selectedBooking, onSelectBooking }) => (
-  <table className="bookings-table">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Time</th>
-        <th>Covers</th>
-        <th>Seated</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        bookings && bookings.map(booking => (
-          <tr key={ booking.id }
-              className={ classnames({
-                cancelled: booking.cancelled,
-                selected: selectedBooking && booking.id === selectedBooking.id
-              }) }
-              onClick={ () => onSelectBooking(booking) }>
-            <td>{ `${ booking.title } ${ booking.firstName } ${ booking.lastName }` }</td>
-            <td>{ booking.time }</td>
-            <td>{ booking.partySize }</td>
-            <td>{ booking.seated ? 'Y' : 'N' }</td>
-          </tr>
-        ))
-      }
-    </tbody>
-  </table>
-);
+@connect(store => {
+  const { bookings: {byId={}, byDate={}}, ui: {selectedDate=null}} = store;
+  const ids = byDate[selectedDate];
+  return {
+    bookings: ids && ids.map(id => byId[id]),
+    selectedBooking: store.ui.selectedBooking
+  };
+}, {
+  selectBooking
+})
+export default class BookingsTable extends React.Component
+{
+  render()
+  {
+    const { bookings, selectedBooking, selectBooking } = this.props;
+
+    return (
+      <table className="bookings-table">
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Time</th>
+          <th>Covers</th>
+          <th>Seated</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          bookings && bookings.map(booking => (
+            <tr key={ booking.id }
+                className={ classnames({
+                  cancelled: booking.cancelled,
+                  selected: selectedBooking && booking.id === selectedBooking.id
+                }) }
+                onClick={ () => selectBooking(booking) }>
+              <td>{ `${ booking.title } ${ booking.firstName } ${ booking.lastName }` }</td>
+              <td>{ booking.time }</td>
+              <td>{ booking.partySize }</td>
+              <td>{ booking.seated ? 'Y' : 'N' }</td>
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
+    );
+  }
+
+}
--- src/components/BookingsTableView.jsx
+++ src/components/BookingsTableView.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { selectBooking } from './../data/actions';
-
 import DateSelector from './DateSelector';
 import BookingsTable from './BookingsTable';
 
 
 
 @connect(store => {
-  const { bookings: {byId={}, byDate={}}, ui: {selectedDate=null}} = store;
-  const ids = byDate[selectedDate];
+  const { bookings: {byDate}, ui: {selectedDate}} = store;
   return {
-    bookings: ids && ids.map(id => byId[id]),
-    selectedBooking: store.ui.selectedBooking
+    hasBookings: byDate && byDate[selectedDate] && byDate[selectedDate].length
   };
-}, {
-  selectBooking
 })
 export default class BookingsTableView extends React.Component
 {
   render ()
   {
-    const { bookings, selectedBooking, selectBooking } = this.props;
+    const { hasBookings } = this.props;
 
     return (
       <section>
@@ -30,13 +24,11 @@ export default class BookingsTableView extends React.Component
           <h2 className="bui-app-intro">Bookings for <DateSelector /></h2>
         </label>
         {
-          bookings &&
-            <BookingsTable bookings={ bookings}
-                           selectedBooking={ selectedBooking }
-                           onSelectBooking={ booking => selectBooking(booking) } />
+          hasBookings &&
+            <BookingsTable />
         }
         {
-          !bookings &&
+          !hasBookings &&
             <div>
               <p>Sorry, no bookings available for the selected day</p>
             </div>
