2c52f911a90ecba34ffc8315e9320a8259b6a90d
Jan Borchers <humancatfood@users.noreply.github.com>
2020-03-05 01:55:33 +0000

    delete data-portion of the redux-store

--- src/data/actions.js
+++ src/data/actions.js
@@ -1,31 +1,7 @@
-import { getBookingsForDate, saveBooking } from './bookings-service';
-
-
 export const ACTIONS = {
-  SET_BUSY: 'SET_BUSY',
   SORT_BY: 'SORT_BY',
-
-  LOAD_BOOKINGS: 'LOAD_BOOKINGS',
-  RECEIVE_BOOKINGS: 'RECEIVE_BOOKINGS',
-  UPDATE_BOOKING: 'UPDATE_BOOKING',
 };
 
-
-export const setBusy = busy => () => ({
-  type: ACTIONS.SET_BUSY,
-  payload: {
-    busy,
-  },
-});
-
-
-export const receiveBookings = (date, bookings) => ({
-  type: ACTIONS.RECEIVE_BOOKINGS,
-  payload: {
-    date,
-    bookings,
-  },
-});
 
 
 export const sortBy = sortProp => ({
@@ -34,49 +10,3 @@ export const sortBy = sortProp => ({
     sortProp,
   },
 });
-
-
-export const updateBooking = newBooking => async dispatch => {
-
-  dispatch(setBusy(true));
-
-  try
-  {
-    const booking = await saveBooking(newBooking);
-    dispatch({
-      type: ACTIONS.UPDATE_BOOKING,
-      payload: {
-        booking,
-      },
-    });
-  }
-  catch (e)
-  {
-    console.error(e);
-  }
-  dispatch(setBusy(false));
-
-};
-
-
-export const selectDate = date => async dispatch => {
-
-  dispatch(setBusy(true));
-
-  try {
-    const bookings = await getBookingsForDate(date);
-    if (bookings)
-    {
-      dispatch(receiveBookings(date, bookings.bookings));
-    }
-    else
-    {
-      dispatch(receiveBookings(date, []));
-    }
-  }
-  catch (e) {
-    console.error(e);
-  }
-  dispatch(setBusy(false));
-
-};
--- src/data/reducers.js
+++ src/data/reducers.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import bookings from './reducer--bookings';
 import ui from './reducer--ui';
 
 
 
 export default combineReducers({
-  bookings,
   ui,
 });
