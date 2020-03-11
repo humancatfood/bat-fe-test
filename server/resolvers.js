8d15e38d2a25da7e21ab8e7cfdf0f3359606e62e
Jan Borchers <humancatfood@users.noreply.github.com>
2020-03-11 22:45:30 +0000

    add validation of mutations

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,29 +1,58 @@
+const yup = require('yup');
+
+
+
 const bookings = []; //TODO: get bookings from DB
 const CHANNEL = 'CHANNEL';
+
+const dateInputSchema = yup.string().matches(/^\d\d\d\d-\d\d-\d\d$/);
+
+const bookingInputSchema = yup.object().shape({
+  title: yup.string().required().oneOf(['Mr', 'Ms', 'Miss', 'Mrs']),
+  firstName: yup.string().trim().max(20).required(),
+  lastName: yup.string().trim().max(20).required(),
+  date: dateInputSchema.required(),
+  time: yup.string().required().matches(/^\d\d(:|.)\d\d$/),
+  partySize: yup.number().required(),
+  notes: yup.string().trim().notRequired().max(140).ensure(),
+  seated: yup.boolean().notRequired().default(false),
+  cancelled: yup.boolean().notRequired().default(false),
+});
+
+const idInputSchema = yup.string().required();
+
 
 
 module.exports = {
   Query: {
     bookings: (parent, { date }, { db }) => new Promise((resolve, reject) => {
 
-        if (error) {
-          reject(error);
-        } else {
-          resolve(data);
-        }
-      });
+      dateInputSchema.validate(date)
+        .then(date => {
           db.find({ date }, (error, data) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(data);
+            }
+          });
+        })
+        .catch(reject);
 
     }),
     booking: (parent, { _id }, { db }) => new Promise((resolve, reject) => {
 
-      db.findOne({ _id }, (error, data) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(data);
-        }
-      });
+      idInputSchema.validate(_id)
+        .then(_id => {
+          db.findOne({ _id }, (error, data) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(data);
+            }
+          });
+        })
+        .catch(reject);
 
     }),
   },
@@ -31,13 +60,24 @@ module.exports = {
   Mutation: {
     updateBooking: (root, { _id, booking }, { db }) => new Promise((resolve, reject) => {
 
-      db.update({ _id }, { $set: booking }, {returnUpdatedDocs: true}, (error, numUpdated, data) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(data);
-        }
-      });
+      bookingInputSchema.validate(booking, { abortEarly: false })
+        .then(booking => {
+          return idInputSchema.validate(_id)
+            .then(_id => {
+              db.update(
+                { _id },
+                { $set: booking },
+                {returnUpdatedDocs: true},
+                (error, numUpdated, data) => {
+                  if (error) {
+                    reject(error);
+                  } else {
+                    resolve(data);
+                  }
+                });
+            });
+        })
+        .catch(reject);
 
     }),
 
