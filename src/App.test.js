9875dd9c79525e63b7473baeee439d8e2b545729
Jan Borchers <humancatfood@users.noreply.github.com>
2020-03-15 01:00:26 +0000

    remove unnecessary test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 
 import App from './App';
@@ -7,12 +6,6 @@ import App from './App';
 
 
 describe('App', () => {
-
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
 
   it(`renders as expected`, () => {
     const { container } = render(<App />);
